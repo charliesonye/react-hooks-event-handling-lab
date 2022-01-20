dc5b0be7cccd5fc1a3a12b33533058af99004070
Charles <c_onyemem@live.com>
2022-01-20 15:35:12 -0500

    Commpleted Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+function changeFocus(){
+    console.log('Good!')
+}
+function changeBlur(){
+    console.log('Hey! Eyes on me!')
+}
+
+    return(
+        <button onFocus={changeFocus} onBlur={changeBlur}>Eyes on Me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+
+
+
+function Keypad(){
+
+    function printPasswordProcess(){
+        console.log("Entering password...")
+    }
+    return(
+        <input type="password" onChange={printPasswordProcess}></input>
+    )
+}
+
+export default Keypad;
